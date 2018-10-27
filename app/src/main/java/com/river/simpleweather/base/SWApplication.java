dacaef90e550e588715e4f6df86129d02132f081
ZhangYanPeng <280338973@qq.com>
2018-10-27 14:46:20 +0800

    test

---
+++ app/src/main/java/com/river/simpleweather/base/SWApplication.java
@@ -0,0 +1,22 @@
+package com.river.simpleweather.base;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by ZhangYanPeng on 2018/10/27.
+ */
+public class SWApplication extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+}
